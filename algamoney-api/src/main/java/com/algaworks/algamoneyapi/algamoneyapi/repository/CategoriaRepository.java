237027652508c40e66f913068af9a1d90e579b8e
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-10-25 12:39:27 -0200

    Aula 3.4 - Consultando primeiro recurso com GET

---
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/repository/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.algaworks.algamoneyapi.algamoneyapi.repository;
+
+import com.algaworks.algamoneyapi.algamoneyapi.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
