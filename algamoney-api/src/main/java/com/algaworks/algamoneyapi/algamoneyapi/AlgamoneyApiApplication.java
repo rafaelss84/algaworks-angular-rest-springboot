45a071fefe5296c248e8fb83519757691c6924c3
rafaelss <computacao.rafael@gmail.com>
2017-11-14 14:12:11 -0200

    Aula 7.2 - Profiles do Spring

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/AlgamoneyApiApplication.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/AlgamoneyApiApplication.java
@@ -1,9 +1,12 @@
 package com.algaworks.algamoneyapi.algamoneyapi;
 
+import com.algaworks.algamoneyapi.algamoneyapi.config.property.AlgamoneyApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
 public class AlgamoneyApiApplication {
 
 	public static void main(String[] args) {
--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/token/RefreshTokenPostProcessor.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/token/RefreshTokenPostProcessor.java
@@ -1,5 +1,7 @@
 package com.algaworks.algamoneyapi.algamoneyapi.token;
 
+import com.algaworks.algamoneyapi.algamoneyapi.config.property.AlgamoneyApiProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -18,6 +20,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice
 public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2AccessToken> {
+
+    @Autowired
+    private AlgamoneyApiProperty algamoneyApiProperty;
 
     @Override
     public boolean supports(MethodParameter methodParameter, Class<? extends HttpMessageConverter<?>> aClass) {
@@ -42,7 +47,7 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
     private void adicionarRefreshTokenNoCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp) {
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(false); // TODO: mudar para true em producao
+        refreshTokenCookie.setSecure(algamoneyApiProperty.getSeguranca().isEnableHttps());
         refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
         refreshTokenCookie.setMaxAge(2592000);
         resp.addCookie(refreshTokenCookie);
