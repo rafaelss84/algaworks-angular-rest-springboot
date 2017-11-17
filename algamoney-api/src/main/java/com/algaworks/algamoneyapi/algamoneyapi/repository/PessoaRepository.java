6debf58eb3948acde7be1cc6d7f6e295e246be4e
rafaelss <computacao.rafael@gmail.com>
2017-11-17 14:41:51 -0200

    Aula 7.7 - Desafio: Pesquisa de pessoa

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/repository/PessoaRepository.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/repository/PessoaRepository.java
@@ -1,7 +1,12 @@
 package com.algaworks.algamoneyapi.algamoneyapi.repository;
 
 import com.algaworks.algamoneyapi.algamoneyapi.model.Pessoa;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
+
 }
--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/PessoaResource.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/resource/PessoaResource.java
@@ -6,6 +6,8 @@ import com.algaworks.algamoneyapi.algamoneyapi.repository.PessoaRepository;
 import com.algaworks.algamoneyapi.algamoneyapi.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -27,10 +29,6 @@ public class PessoaResource {
 
     @Autowired
     private ApplicationEventPublisher publisher;
-
-    @GetMapping
-    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
-    public List<Pessoa> listar() {return pessoaRepository.findAll();}
 
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -66,5 +64,11 @@ public class PessoaResource {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
         pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+    public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+        return pessoaRepository.findByNomeContaining(nome, pageable);
     }
 }
