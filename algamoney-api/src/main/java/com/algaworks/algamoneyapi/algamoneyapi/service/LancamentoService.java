aac276c1b5611af7fe183f4a9917f6c4eac0b16f
rafaelss <computacao.rafael@gmail.com>
2017-11-20 12:57:38 -0200

    Aula 7.9 - Desafio: Atualizar Lancamento

--- algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/service/LancamentoService.java
+++ algamoney-api/src/main/java/com/algaworks/algamoneyapi/algamoneyapi/service/LancamentoService.java
@@ -5,6 +5,7 @@ import com.algaworks.algamoneyapi.algamoneyapi.model.Pessoa;
 import com.algaworks.algamoneyapi.algamoneyapi.repository.LancamentoRepository;
 import com.algaworks.algamoneyapi.algamoneyapi.repository.PessoaRepository;
 import com.algaworks.algamoneyapi.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,36 @@ public class LancamentoService {
             throw  new PessoaInexistenteOuInativaException();
         }
         return lancamentoRepository.save(lancamento);
+    }
+    
+    public Lancamento atualizar(Long codigo, Lancamento lancamento) {
+        Lancamento lancamentoSalvo = buscarLancamentoExistente(codigo);
+        if (!lancamento.getPessoa().equals(lancamentoSalvo.getPessoa())) {
+            validarPessoa(lancamento);
+        }
+
+        BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo");
+        
+        return lancamentoRepository.save(lancamentoSalvo);
+    }
+
+    private void validarPessoa(Lancamento lancamento) {
+        Pessoa pessoa = null;
+        if (lancamento.getPessoa().getCodigo() != null) {
+            pessoa = pessoaRepository.findOne(lancamento.getPessoa().getCodigo());
+        }
+
+        if (pessoa == null || pessoa.isInativo()) {
+            throw new PessoaInexistenteOuInativaException();
+        }
+    }
+
+    private Lancamento buscarLancamentoExistente(Long codigo) {
+        Lancamento lancamentoSalvo = lancamentoRepository.findOne(codigo);
+        if (lancamentoSalvo == null) {
+            throw new IllegalArgumentException();
+        }
+        return lancamentoSalvo;
     }
 
 }
