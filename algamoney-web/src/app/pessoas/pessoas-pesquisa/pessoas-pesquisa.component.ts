f62f612301df05fbd681158a99871263fd3c0f72
rafaelss <computacao.rafael@gmail.com>
2018-01-25 17:19:00 -0200

    Aula 17.13. Desafio: implementando a exclusão de pessoas

--- algamoney-web/src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ algamoney-web/src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { PessoaFiltro, PessoaService } from '../pessoa.service';
 import { LazyLoadEvent } from 'primeng/components/common/lazyloadevent';
+import { ErrorHandlerService } from '../../core/error-handler.service';
+import { ConfirmationService } from 'primeng/components/common/api';
+import { ToastyService } from 'ng2-toasty';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -11,8 +14,13 @@ export class PessoasPesquisaComponent {
   totalRegistros = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabela') grid;
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(
+    private pessoaService: PessoaService,
+    private errorHandler: ErrorHandlerService,
+    private confirmation: ConfirmationService,
+    private toasty: ToastyService) { }
 
   pesquisar(pagina = 0) {
     this.filtro.pagina = pagina;
@@ -21,11 +29,35 @@ export class PessoasPesquisaComponent {
       .then(resultado => {
         this.totalRegistros = resultado.total;
         this.pessoas = resultado.pessoas;
-      });
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(pessoa: any) {
+    this.confirmation.confirm({
+      message: 'Tem certeza que deseja excluir?',
+      accept: () => {
+        this.excluir(pessoa);
+      }
+    });
+  }
+
+  excluir(pessoa: any) {
+    this.pessoaService.excluir(pessoa.codigo)
+      .then(() => {
+        if (this.grid.first === 0) {
+          this.pesquisar();
+        } else {
+          this.grid.first = 0;
+        }
+
+        this.toasty.success('Pesssoa excluída com sucesso!');
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 }
