bab5b8ea147d70f89b5621666fbd2cbfeed0d1d6
rafaelss <computacao.rafael@gmail.com>
2018-01-26 12:31:25 -0200

    Aula 17.21. Desafio: implementando o cadastro de pessoas

--- algamoney-web/src/app/pessoas/pessoa.service.ts
+++ algamoney-web/src/app/pessoas/pessoa.service.ts
@@ -2,6 +2,7 @@ import { Http, Headers, URLSearchParams } from '@angular/http';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/toPromise';
+import { Pessoa } from '../core/model';
 
 export class PessoaFiltro {
   nome: string;
@@ -71,4 +72,14 @@ export class PessoaService {
       .toPromise()
       .then(() => null);
   }
-}
+
+  adicionar(pessoa: Pessoa): Promise<Pessoa> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.post(`${this.pessoasUrl}`, JSON.stringify(pessoa), { headers })
+      .toPromise()
+      .then(response => response.json());
+  }
+ }
