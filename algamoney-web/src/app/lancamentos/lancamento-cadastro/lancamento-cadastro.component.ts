9f1d2de02b38284e475b46811e109858f940835d
rafaelss <computacao.rafael@gmail.com>
2018-01-26 16:38:11 -0200

    Aula 18.8. Salvando lançamentos editados

--- algamoney-web/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ algamoney-web/src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -57,11 +57,28 @@ export class LancamentoCadastroComponent implements OnInit {
   }
 
   salvar(form: FormControl) {
+    if (this.editando) {
+      this.atualizarLancamento(form);
+    } else {
+      this.adicionarLancamento(form);
+    }
+  }
+
+  adicionarLancamento(form: FormControl) {
     this.lancamentoService.adicionar(this.lancamento)
       .then(() => {
         this.toasty.success('Lançamento adicionado com sucesso!');
         form.reset();
         this.lancamento = new Lancamento();
+      })
+      .catch(erro => this.errorHandler.handle(erro));
+  }
+
+  atualizarLancamento(form: FormControl) {
+    this.lancamentoService.atualizar(this.lancamento)
+      .then(lancamento => {
+        this.toasty.success('Lançamento alterado com sucesso!');
+        this.lancamento = lancamento;
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
