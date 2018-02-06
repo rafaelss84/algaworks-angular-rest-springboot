ae497390e66f37a2138dbe03df3bfc908d555017
rafaelss <computacao.rafael@gmail.com>
2018-02-06 12:48:54 -0200

    Aula 19.8. Exibindo o nome do usuário logado

--- algamoney-web/src/app/core/navbar/navbar.component.ts
+++ algamoney-web/src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../seguranca/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,7 @@ export class NavbarComponent implements OnInit {
 
   exibindoMenu = false;
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
