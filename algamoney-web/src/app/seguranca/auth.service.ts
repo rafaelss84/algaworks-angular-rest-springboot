3092418789fbc5247a76afd8b48d3ebe6cbd5ad8
rafaelss <computacao.rafael@gmail.com>
2018-02-06 13:08:39 -0200

    Aula 19.10. Obtendo um novo access token

--- algamoney-web/src/app/seguranca/auth.service.ts
+++ algamoney-web/src/app/seguranca/auth.service.ts
@@ -24,7 +24,8 @@ export class AuthService {
 
     const body = `username=${usuario}&password=${senha}&grant_type=password`;
 
-    return this.http.post(this.oauthTokenUrl, body, { headers })
+    return this.http.post(this.oauthTokenUrl, body,
+        { headers, withCredentials: true })
       .toPromise()
       .then(response => {
         this.armazenarToken(response.json().access_token);
@@ -39,6 +40,27 @@ export class AuthService {
         }
 
         return Promise.reject(response);
+      });
+  }
+
+  obterNovoAccessToken(): Promise<void> {
+    const headers = new Headers();
+    headers.append('Authorization', 'Basic YW5ndWxhcjpAbmd1bEByMA==');
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    const body = 'grant_type=refresh_token';
+
+    return this.http.post(this.oauthTokenUrl, body,
+        { headers, withCredentials: true })
+      .toPromise()
+      .then(response => {
+        this.armazenarToken(response.json().access_token);
+        console.log('Novo access token criado!');
+        return Promise.resolve(null);
+      })
+      .catch(response => {
+        console.error('Erro ao renovar token.', response);
+        return Promise.resolve(null);
       });
   }
 
